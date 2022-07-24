fe81ccf1b3211488102f6ac7cdeb304e4ce93e96
Solomdev <70018671+SolomDev@users.noreply.github.com>
2022-07-24 07:26:15 +0200

    Create secondtask.js

---
+++ secondtask.js
@@ -0,0 +1,22 @@
+const member = {
+    age: 30,
+    working: false,
+    country: "Egypt",
+    hobbies: ["Reading", "Swimming", "Programming"],
+};
+
+//Statup
+const a = '30';
+const w = false;
+const c = 'Egypt';
+const h1 = 'Reading';
+const h3 = 'Programming';
+
+console.log(`My Age Is ${a} And Iam ${w ? "" : "Not"} Working`);
+// My Age Is 30 And Iam Not Working
+
+console.log(`I Live in ${c}`);
+// I Live in Egypt
+
+console.log(`My Hobbies: ${h1} And ${h3}`);
+// My Hobbies: Reading and Programming
